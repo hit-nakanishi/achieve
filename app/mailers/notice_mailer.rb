fa3623df8704443ef5cb652a4f74ff6d028aaf2e
hitoshi nakanishi <naka24naka24@gmail.com>
2016-07-22 01:34:55 +0000

    DIVE11_課題提出1

--- app/mailers/notice_mailer.rb
+++ app/mailers/notice_mailer.rb
@@ -1,10 +1,20 @@
 class NoticeMailer < ApplicationMailer
 
+  # ブログ投稿
   def sendmail_blog(blog)
     
     @blog = blog
 
     mail to: "naka24naka24@gmail.com",
          subject: '【Achieve】ブログが投稿されました'
+  end
+
+  # お問い合わせ投稿
+  def sendmail_contact(contact)
+
+    @contact = contact
+
+    mail to: "naka24naka24@gmail.com",
+         subject: '【Achieve】お問い合わせが届きました'
   end
 end
