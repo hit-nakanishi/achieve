c7699127ba941deb022a6dad5a4ae6e4050424d3
hitoshi nakanishi <naka24naka24@gmail.com>
2016-08-05 01:39:59 +0000

    DIVE18_課題提出1_0805

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,8 @@
 class Task < ActiveRecord::Base
   belongs_to :user
   belongs_to :charge, class_name: 'User', foreign_key: 'charge_id'
+  
   validates :title, presence: true
 
+  has_many :submit_requests, dependent: :destroy
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -17,6 +17,8 @@ class User < ActiveRecord::Base
   has_many :followers, through: :reverse_relationships, source: :follower
   
   has_many :tasks, dependent: :destroy
+
+  has_many :submit_request, dependent: :destroy
   
   def self.find_for_facebook_oauth(auth, signed_in_resource=nil)
     user = User.where(provider: auth.provider, uid: auth.uid).first
@@ -74,4 +76,9 @@ class User < ActiveRecord::Base
   def unfollow!(other_user)
     relationships.find_by(followed_id: other_user.id).destroy
   end
+
+  def friend
+    followers & followed_users
+  end
+  
 end
