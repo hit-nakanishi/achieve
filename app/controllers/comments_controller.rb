17f0c9eccc4a5c0410ddcc3f7f6ea04dd0d92d9b
hitoshi nakanishi <naka24naka24@gmail.com>
2016-08-10 04:20:01 +0000

    DIVE19_課題提出6_0810

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -13,14 +13,19 @@ class CommentsController < ApplicationController
     # クライアント要求に応じてフォーマットを変更
     respond_to do |format|
       if @comment.save
-#        format.html { redirect_to blog_path(@blog), notice: 'コメントを投稿しました。' }
-#        format.json { render :show, status: :created, location: @comment }
         format.js { render :index }
-      else
-#        format.html { render :new }
-#        format.json { render json: @comment.errors, status: :unprocessable_entity }
       end
     end
+#    respond_to do |format|
+#      if @comment.save
+#        format.html { redirect_to blog_path(@blog), notice: 'コメントを投稿しました。' }
+#        format.json { render :show, status: :created, location: @comment }
+#        format.js { render :index }
+#      else
+#        format.html { render :new }
+#        format.json { render json: @comment.errors, status: :unprocessable_entity }
+#      end
+#    end
   end
 
   def destroy
