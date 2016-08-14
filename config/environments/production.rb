2cb653a5b3a9a95b9a0c986ba20d2b634a51c122
hit-nakanishi <naka24naka24@gmail.com>
2016-08-14 11:38:24 +0000

    init unicorn4

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   
-  config.action_mailer.default_url_options = { host: 'mysterious-fortress-93227.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: '52.197.66.53' }
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings =
   {
